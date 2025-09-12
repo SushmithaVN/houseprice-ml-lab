a8867d89c0165aad96a3b8b02fb960470313e602
SushmithaVN <sushmithavn15@gmail.com>
2025-09-12 08:40:09 +0530

    initial pipeline version done

--- app/app.py
+++ app/app.py
@@ -4,22 +4,57 @@ import pandas as pd
 
 st.title("🏠 House Price Predictor")
 
-model = joblib.load("model.pkl")
+model = joblib.load("../model.pkl")
+
+locality_options = [
+    'BTM Layout',
+    'Attibele',
+    'K R Puram',
+    'Marathahalli',
+    'Indiranagar',
+    'Electronic City',
+    'Yalahanka',
+    'Malleshwaram',
+    'Jayanagar'
+]
+locality_options = sorted(locality_options)
+
+facing_options = sorted([
+    'North-West',
+    'East',
+    'North',
+    'West',
+    'North-East',
+    'South-East',
+    'South',
+    'Missing'
+])
+
+parking_options = sorted([
+    'Bike',
+    'Bike and Car',
+    'Car',
+    'Missing'
+])
 
 with st.form("predict"):
     rent = st.number_input("Monthly Rent", value=15000)
     sqft = st.number_input("Square Feet", value=1000)
-    locality = st.text_input("Locality (e.g., Downtown)", "Downtown")
+    locality = st.selectbox("Locality",locality_options,index=0)
     bedrooms = st.number_input("Bedrooms", value=2, min_value=0, max_value=10, step=1)
-    parking = st.text_input("Parking (Open/Covered/Missing)", "Open")
+    bathrooms = st.number_input("Bathrooms", value=2, min_value=0, max_value=10, step=1)
+    facing=st.selectbox("Facing",facing_options,index=0)
+    parking = st.selectbox("Parking", parking_options,index=0)
     submitted = st.form_submit_button("Predict")
 
 if submitted:
     X = pd.DataFrame([{
         "rent": rent,
-        "sqft": sqft,
+        "area": sqft,
         "locality": locality if locality else "Missing",
-        "bedrooms": bedrooms,
+        "BHK": bedrooms,
+        "bathrooms": bathrooms,
+        "facing":facing if facing else "Missing",
         "parking": parking if parking else "Missing"
     }])
     pred = model.predict(X)[0]
